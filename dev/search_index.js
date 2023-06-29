e1caeef0f0c5899aaf35b06cddb92e9056a40406
Documenter.jl <documenter@juliadocs.github.io>
2023-06-29 13:18:44 +0000

    build based on 1830e0b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TKCBED","category":"page"},{"location":"#TKCBED","page":"Home","title":"TKCBED","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TKCBED.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TKCBED]","category":"page"}]
+}
